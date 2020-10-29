dbf76915c72ad321c193bde4405688ac4579eb20
Michał Starski <michalstarski99@gmail.com>
2020-10-29 19:52:36 +0100

    Added setup.cfg, updated README and setup.py - getting ready to deploy

--- README.md
+++ README.md
@@ -4,4 +4,14 @@ A simple CLI for listing and executing npm tasks.
 
 ## Installation
 
+```bash
+$ pip install nodetasks
+```
+
 ## Usage
+
+![Example usage](example.gif)
+
+## Todo
+
+- [] Give user ability to run tasks with yarn
--- setup.py
+++ setup.py
@@ -4,18 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="node-tasks-michalStarski",
+    name="nodetasks",
     version="0.0.1",
     author="Michał Starski",
     author_email="michal.starski@pm.me",
-    description="Simple CLI script for listing and running npm/yarn tasks",
+    description="Simple CLI script for listing and running npm tasks",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/michalStarski/node-tasks",
+    url="https://github.com/michalStarski/nodetasks",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: MIT License",
-        "Operating System :: OS Independent"
     ],
     python_requires='>=3.6',
     install_requires=[
